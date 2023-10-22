a65b8b6ba4fd9b2bc08fb3570556a96a4cf5938b
Aslbek Kucharov <aslbek@bulavka.uz>
2023-10-22 15:15:04 +0500

    feat: moved from html parsing mode to markdown2 parsing mode to use new feature of telegram (code highlighting) that will be available soon

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,18 +5,26 @@ import { LibOptionsTypes, LogMessageType, LogOptions } from "src/types"
 class LoggerPlugin {
 
     private bot: Telegram
+    private codeLang: string
 
     constructor(options: LibOptionsTypes) {
         this.bot = new Telegram(options)
+        this.codeLang = options.codeLang || 'js'
     }
 
     public sendLog(message: LogMessageType, options?: LogOptions) {
 
         if (options?.shouldBeFormatted) {
+            let codeLang = this.codeLang
 
             message = pretty(message)
 
-            return this.bot.sendMessage('<code>' + message + '</code>', { parse_mode: 'html' })
+            if (options.lang) {
+                codeLang = options.lang
+            }
+
+            
+            return this.bot.sendMessage("```" + `${codeLang}\n` + message + "\n```", { parse_mode: 'MarkdownV2' })
         }
 
         return this.bot.sendMessage(message)
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,8 +3,10 @@ export type LogMessageType = string | object
 export interface LibOptionsTypes {
     token: string
     chatId: string | number
+    codeLang?: string
 }
 
 export interface LogOptions {
     shouldBeFormatted?: boolean
+    lang?: string
 }
